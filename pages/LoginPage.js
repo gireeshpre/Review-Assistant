b9f28382675b8105646f33056cb22ff4c590469f
gireeshpre <gnadiminti@presidio.com>
2026-08-10 13:02:17 +0530

    intentionally added bad code (#1)
    * intentionally added bad code
    
    * remoived bad code
    
    * removed hardcoded waits

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -5,41 +5,36 @@ import BasePage from './BasePage.js';
 export default class LoginPage extends BasePage {
 
     constructor(page) {
-
         super(page);
-
         this.usernameTextbox = page.locator('input[name="username"]');
         this.passwordTextbox = page.locator('input[name="password"]');
         this.loginButton = page.locator('button[type="submit"]');
     }
 
     async enterUsername(username) {
-        
         await this.enterText(this.usernameTextbox, username);
-        
     }
 
     async enterPassword(password) {
-
         await this.enterText(this.passwordTextbox, password);
-
     }
 
     async clickLoginButton() {
-
         await this.click(this.loginButton);
-
     }
 
     async login(username, password) {
-
         await this.enterUsername(username);
-        await this.wait(2);
+       
         await this.enterPassword(password);
-        await this.wait(2);
+       
         await this.clickLoginButton();
-        await this.wait(5);
+       
+
+        // intentionally added bad code
+        //  await this.wait(2);
+
+       
 
     }
-
 }
