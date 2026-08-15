6d5a3a118aea9476cd27f32713f2dd64b254e81b
gireeshpre <gnadiminti@presidio.com>
2026-08-15 05:34:11 +0530

    added login practice test

---
+++ tests/ai_practice_login.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('login test', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.waitForTimeout(5000);
+
+    await page.locator('#user-name').fill('standard_user');
+
+    await page.locator('#password').fill('secret_sauce');
+
+    await page.locator('//input[@type="submit"]').click();
+
+});
