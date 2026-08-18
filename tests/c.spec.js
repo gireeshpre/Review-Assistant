27272958aaeea7cedaf5951c2b3f9ed388b40749
gireeshpre <gnadiminti@presidio.com>
2026-08-18 11:15:38 +0530

    BadCodeFix: Fix c.spec.js Playwright issues (#7)
    * file c is commited
    
    * BadCodeFix

---
+++ tests/c.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Login with valid credentials navigates to inventory page', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    await expect(page.getByRole('heading', { name: 'Products' })).toBeVisible();
+
+});
